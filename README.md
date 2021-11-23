0213c5465eef4b21670830b8dc3af97ad233a1a8
OnurSefa <onursefa_ozcibik@hotmail.com>
2021-11-23 04:37:02 +0300

    basic clip operations

--- README.md
+++ README.md
@@ -7,6 +7,12 @@
 Project aim is to prepare a sondbox in which we can test our knowledge
 about VQGAN and CLIP.
 
+
+### Useful Links
+
+- [CLIP Github Page](https://github.com/openai/CLIP)
+
+
 ### Authors
 
 - [Onur Sefa Ozcibik](https://github.com/OnurSefa)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import torch.optim as optim
 import torch.nn as nn
 import clip
 from matplotlib.image import imread
+from PIL import Image
 
 # fasttext.util.download_model('en', if_exists='ignore')
 # ft = fasttext.load_model('cc.en.300.bin')
@@ -40,13 +41,18 @@ def create_model(given_words, l_rate=0.003, e=200):
 
 
 if __name__ == '__main__':
-    random_image = np.random.randint(0, 256, (64, 64, 3))
-    show_image(random_image)
+    # random_image = np.random.randint(0, 256, (64, 64, 3))
+    # show_image(random_image)
     model, preprocess = clip.load("ViT-B/32", device=device)
-    words = ['cat', 'dog', 'elephant']
-    cat_image = imread('cat.jpg')
-    dog_image = imread('dog.jpg')
-    ele_image = imread('ele.jpg')
+    # words = ['cat', 'dog', 'elephant']
+    # cat_image = imread('cat.jpg')
+    # dog_image = imread('dog.jpg')
+    # ele_image = imread('ele.jpg')
+
+    cat = preprocess(Image.open('ele.jpg')).unsqueeze(0).to(device)
+    cat_text = clip.tokenize(['an elephant', 'a dog', 'a cat', 'a tree', 'a monkey', 'soul', 'pencil']).to(device)
+    a = model(cat, cat_text)
+    print(a)
 
     # words = ['cat']
     # cat_word = ft.get_word_vector('cat')
