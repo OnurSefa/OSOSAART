95c872f2a7263822d091dab66b759601f7f8a415
OnurSefa <onursefa_ozcibik@hotmail.com>
2021-11-23 04:47:37 +0300

    Fasttext related codes and requirements deleted. No more need to it.

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import fasttext.util
 import torch
-import torch.optim as optim
 import torch.nn as nn
 import clip
 from matplotlib.image import imread
-
-# fasttext.util.download_model('en', if_exists='ignore')
-# ft = fasttext.load_model('cc.en.300.bin')
 
 device = "cuda" if torch.cuda.is_available() else 'cpu'
 
@@ -42,16 +37,6 @@ def create_model(given_words, l_rate=0.003, e=200):
 if __name__ == '__main__':
     random_image = np.random.randint(0, 256, (64, 64, 3))
     show_image(random_image)
-    model, preprocess = clip.load("ViT-B/32", device=device)
-    words = ['cat', 'dog', 'elephant']
-    cat_image = imread('cat.jpg')
-    dog_image = imread('dog.jpg')
-    ele_image = imread('ele.jpg')
-
-    # words = ['cat']
-    # cat_word = ft.get_word_vector('cat')
-    # print(cat_word)
-
 
 
 
