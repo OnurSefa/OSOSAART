659834f1bd42d9eae8bb67ec5df36302d54353fe
OnurSefa <onursefa_ozcibik@hotmail.com>
2022-01-18 09:33:28 +0300

    HD versions of the images are collected

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ import urllib.request
 if __name__ == '__main__':
     first_page_response = requests.get('https://www.deviantart.com/search?q=fantasy%20landscape')
     html_result = html.unescape(first_page_response.text)
-
-    p = re.compile('<img loading="lazy".*? src="(.*?)"')
+    # with open('third.html', 'w') as output_file:
+    #     output_file.write(html_result)
+    p = re.compile('<a data-hook="deviation_link".*?href="(.*?)"')
     result = p.findall(html_result)
-    print(result)
     directory = './images/'
     for r in result:
-        urllib.request.urlretrieve(r, directory+r.split('/')[-1].split('?')[0])
+        deviation_response = requests.get(r)
+        deviation_result = html.unescape(deviation_response.text)
+        # with open('second.html', 'w', encoding='utf-8') as output_file:
+        #     output_file.write(deviation_result)
+        l = re.compile('<img.*?class="_3X6pY".*?src="(.*?)"')
+        l_result = l.findall(deviation_result)
+        for l_instance in l_result:
+            name = directory+l_instance.split('?')[0].split('/')[-1]
+            urllib.request.urlretrieve(l_instance, name)
 
     # with open('first_page.html', 'w') as output_file:
     #     output_file.write(html.unescape(first_page_response.text))
