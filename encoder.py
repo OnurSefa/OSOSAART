0804cc863707381cebb194a6187c0a995c71809f
sseff <sefikaakman4@hotmail.com>
2022-02-09 20:57:02 +0300

    added labels and encoder

--- encoder.py
+++ encoder.py
@@ -1,4 +1,75 @@
 
+import io
+import numpy as np
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+import handler
+import torch.nn as nn
+from sklearn.cluster import MiniBatchKMeans
+
+class Ososa(nn.Module):
+    def __init__(self):
+        super(Ososa, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, (10, 10))
+        self.conv2 = nn.Conv2d(6, 4, (8, 8))
+        self.conv3 = nn.Conv2d(4, 2, (6, 6))
+        self.relu = nn.ReLU()
+        self.pool1 = nn.MaxPool2d((3, 3))
+        self.pool2 = nn.MaxPool2d((3, 3))
+        self.pool3 = nn.MaxPool2d((5, 5))
+        self.linear1 = nn.Linear(2 * 5 * 5, 10)
+        self.linear2 = nn.Linear(10, 2)
 
 
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool2(x)
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.pool3(x)
+        x = torch.flatten(x, 1)
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+        x = self.relu(x)
+        return x
 
+
+def loss_function(p, y):
+    cluster_model = MiniBatchKMeans(n_clusters=5)
+    y_hat = cluster_model.fit_predict(p)
+    print("a")
+
+
+def train(x, y, epoch=200, learning_rate=0.005):
+    model = Ososa()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    loss_f = loss_function
+    loss_fn = nn.CrossEntropyLoss()
+    y = y.long()
+
+    # TODO batch halinde alabiliriz daha cok data kullandigimizda
+    for e in range(epoch):
+        prediction = model(x)
+        loss = loss_f(prediction, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if e % 10 == 0:
+            print("Loss in e={} is {}".format(e, loss))
+        
+
+if __name__ == '__main__':
+    data, labels = handler.take_data()
+    data = torch.from_numpy(data)
+    labels = torch.from_numpy(labels)
+    data = data.float()
+    labels = labels.float()
+    train(data, labels)
+    print('b')
--- handler.py
+++ handler.py
@@ -2,7 +2,7 @@
 
 from PIL import Image
 import os
-
+import numpy as np
 
 
 def resize_images(input_directory="./input", output_directory="./output", neu_size=(300,300)):
@@ -15,9 +15,40 @@ def resize_images(input_directory="./input", output_directory="./output", neu_si
 			im = Image.open("{}/{}/{}".format(input_directory, directory_name, image_name))
 			neu_im = im.resize(neu_size)
 			neu_im.save("{}/{}/{}".format(output_directory, directory_name, image_name))
-	
+
+
+styles = ["abstract", "animal-painting", "cityscape", "figurative", "flower-painting"]
+
+def image_to_numpy(input_directory="./output", output_directory="./numpy_images", label_directory="./labels"):
+	directory_list = os.listdir(input_directory)
+	for directory_name in directory_list:
+		image_name_list = os.listdir("{}/{}".format(input_directory, directory_name))
+		
+		for image_name in image_name_list:
+			im = Image.open("{}/{}/{}".format(input_directory, directory_name, image_name))
+			np_value = np.array(im)
+			label = styles.index(directory_name)
+			# np_result = np.append(np_value, label)
+			with open("{}/{}".format(label_directory, image_name.split(".")[0] + ".npy"), 'wb') as output_file:
+				np.save(output_file, np.array(label))
+			with open("{}/{}".format(output_directory, image_name.split(".")[0] + ".npy"), 'wb') as output_file:
+				np.save(output_file, np_value)
+
+def take_data(input_directory_1="./numpy_images", input_directory_2="./labels"):
+	directory_list = os.listdir(input_directory_1)
+	data = []
+	labels = []
+	for image_name in directory_list:
+		datum = np.load("{}/{}".format(input_directory_1, image_name))
+		datum = datum.reshape(3, 300, 300)
+		label = np.load("{}/{}".format(input_directory_2, image_name))
+		data.append(datum)
+		labels.append(label)
+	return np.array(data), np.array(labels)
+
+    
 
 if __name__ == '__main__':
-	resize_images()
-
+	x, y = take_data()
+	# print("a")
 
